5c9353fa3a397fe6f59a31180bc62facec2c37b6
Shane Anderson <shane@Shanes-MacBook-Air.local>
2025-11-04 21:11:34 +0800

    Init

---
+++ nvim/lua/options.lua
@@ -0,0 +1,36 @@
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+vim.cmd("set signcolumn=yes:1")
+
+-- Tab
+vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.softtabstop = 2 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- UI config
+vim.opt.number = true -- show absolute number
+vim.opt.relativenumber = false -- add numbers to each line on the left side
+vim.opt.cursorline = false -- highlight cursor line underneath the cursor horizontally
+vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
+
+-- Searching
+vim.opt.incsearch = true -- search as characters are entered
+vim.opt.hlsearch = false -- do not highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+
+-- Keep cursor place
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,145 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+-- 
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     -- priority = 1000,
+--     config = function()
+--       require("catppuccin").setup({
+--         flavour = "mocha",   -- latte, frappe, macchiato, mocha
+--         transparent_background = true,
+--       })
+-- 
+--       -- setup must be called before loading
+--       vim.cmd.colorscheme "catppuccin"
+--     end,
+--   },
+-- 
+  {
+    'Mofiqul/vscode.nvim',
+     config = function()
+       require("vscode").setup({
+         transparent = true,
+       })
+
+       -- setup must be called before loading
+       vim.cmd.colorscheme "vscode"
+     end,
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+    opts = {
+      disable_filetype = { "TelescopePrompt", "vim" },
+    }
+  },
+
+  	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+
+			-- Simple and easy statusline.
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+		end,
+	},
+
+	{ -- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"java",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = { "latex" },
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+		},
+	},
+
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'enter',
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
+
+      completion = {
+        documentation = { auto_show = true },
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+
+    opts_extend = { "sources.default" }
+  },
+
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+})
